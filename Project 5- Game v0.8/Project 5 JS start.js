0e6af183b6c6a0c0f98f9a560e354389de36d021
Nick Esposito <31749740+nickesposito@users.noreply.github.com>
2017-11-10 18:17:58 -0500

    trying to slim code down
    Trying to use for loops and logic for this, will reduce code Drastically. May introduce a matrix for navigation to improve it, rather than having multiple functions like in previous versions. Inventory/Take is complete from last project, will try to "streamline" take function. This project is about cutting excess code, and finding the right balance of logic to use will help me achieve that goal. For example, incorporating the HasVistedLoc is location prototypes will the amount of global variables I will need, and it will heavily reduce ScoreTime function, which will make the code look cleaner.

--- Project 5- Game v0.8/Project 5 JS start.js
+++ Project 5- Game v0.8/Project 5 JS start.js
@@ -1,9 +1,25 @@
 //Project 5
 //NE
-//11717
+//LAST UPDATED 111017
 ///////////////////////
 ///////////////////////
 ///////////////////////
+//New Global Variables
+var currentLoc = 0;
+var nextcurrentLoc = 0;
+var score = 0;
+var userInventory = []
+
+
+
+
+
+
+
+
+
+
+//
 //Location function
 function location (id, name, desc, item){
 	this.id = id;
@@ -28,7 +44,66 @@ var BillyBobs = new location (8. "BillyBobs", "You head over to Vassar now to se
 var FratHouse = new location (9, "Frat House", "You stop by the frat house, where the party gets shut down and dispersed two hours in.", false);
 //Array for locations
 var location = [UpperNew,Darbys,Donnelly,Union,RiverStation,ClubTT,Gias,Amicis,BillyBobs,FratHouse]
-//
+//Movement 
+//reworking this BIGLY!!!!!!!:)
+//Each array reps currentLoc, value represents movement based on N,S,E,W btn click, 0-9 loc in ord
+var MoveTimeButtons = new Array ("btnNorth","btnSouth","btnEast","btnWest");
+////
+var Movement = new Array ( [1,2,3,4],				
+						   [-1,0,8,-1],
+						   [0,-1,6,4],
+						   [5,6,9,0],
+						   [-1,7,0,-1],
+						   [8,3,-1,1],
+						   [3,-1,-1,2],
+						   [4,-1,2,-1],
+						   [-1,5,-1,-1],
+						   [-1,-1,-1,3]
+						   )
+//create array to enable/disable buttons!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Introducing basic logic, will be a lot less code than if/else and switch-case
+//create one function to control btn clicks
+function ButtonTime() {
+	if  (user === "N" || user === "n") {
+		user = north	
+	} else if (user === "S" || user ==="s"){
+		user = south
+	} else if (user === "E" || user ==="e"){
+		user = east
+	} else if (user === "W" || user ==="w") {
+		user = west;
+	}
+	nextcurrentLoc = Movement[currentLoc][user];
+	
+
+
+
+
+
+
+
+
+
+
 /* for loop
 
 
@@ -43,7 +118,6 @@ function item (id, name, desc) {
 	this.id = id;
 	this.name = name;
 	this.desc = desc;
-	this.taken = false;
 	function toString() {
 		return desc;
 	}
@@ -57,11 +131,4 @@ var items = [beer,pizza,beerMoney]
 //
 //
 
-
-
-
-
-
-//function MoveTime()
-function MoveTime() {
 	
