b100890ef122334e2ffa650de591189bd598fb5b
nickesposito <nick.esposito.j@gmail.com>
2017-11-15 23:49:13 -0500

    Progress
    Buttons disable/enable now through a matrix, and a function with a for loop to enable/diable at current locations. Working on a few bugs still, error message is not displaying and trying to update my scoretime function. Reduced code a decent amount at this point.

--- Project 5- Game v0.8/Project 5 JS.js
+++ Project 5- Game v0.8/Project 5 JS.js
@@ -1,6 +1,8 @@
-//Project 5
+//Project 5 Javascript
 //NE
-//LAST UPDATED 111417
+//Nick Esposito
+//LAST UPDATED 11-15-2017
+//Project 5 - Game v0.8
 ///////////////////////
 ///////////////////////
 ///////////////////////
@@ -28,7 +30,7 @@ function loc (id, name, desc, item){
 //Declaring locations (Id,name,desc. of location, item or not)
 var UpperNew = new loc (0, "Upper New", "You're back at Upper New. Go back out again or call it a night!", null);
 var Darbys = new loc (1, "Darbys", "You guys arrive at Darby O'Gills, and the place is packed beacuse its 2 for $1 night!", null);
-var Donnelly = new loc (2, "Donnelly Hall", "You are at Donnelly Hall hailing a cab, see where that takes you!", null);
+var Donnelly = new loc (2, "Donnelly Hall", "You are at Donnelly Hall hailing a cab, see where that takes you! But first, you meet your friend with your new ID!", fakeId);
 var Union = new loc (3, "Union", "You walked a few blocks, and now you are at Union Tavern, a LEGNENDARY bar! But the bar has a HUGE line. Should we wait or stay? Your call.", null);
 var RiverStation = new loc (4, "River Station", "You now go to River Station, and you found five dollars on the ground in line. TAKE it!", beerMoney);
 var ClubTT = new loc (5, "Club 33", "You reach Club 33, the up and coming alternative to Union. Only upside is they're offering FREE BEER! TAKE the beer!", beer);
@@ -52,20 +54,23 @@ function item (id, name, desc) {
 var beer = new item (5, "Beer", "This Bud Light is Rocky Mountain Cold!", false);
 var pizza = new item (6, "Pizza", "Gia's pizza is the freaking BOMB!", false);
 var beerMoney = new item (4, "Beer Money", "Sweet! Five Dollars!", false);
+var fakeId = new item (2, "Fake ID", "Your fake looks good! It's one of the new versions from Connecticut!", false);
 //Array for items
 //Numbers are currentLoc where items are
 var items = new Array();
 items[5]= beer
 items[6]= pizza;
 items[4]= beerMoney;
+items[2]= fakeId;
 //Movement 
 //reworking this BIGLY!!!!!!!:)
 //Each array reps currentLoc, value represents movement based on N,S,E,W btn click, 0-9 loc in order
 ////
+var btn = ["btnNorth","btnSouth","btnEast","btnWest"]
 var Movement = [           /*N S E W */
 						   [1,2,3,4],	//0			
 						   [-1,0,5,-1],	//1
-						   [0,-1,6,4],	//2
+						   [0,-1,6,7],	//2
 						   [5,6,9,0],	//3
 						   [-1,7,0,-1],	//4
 						   [8,3,-1,1],	//5
@@ -74,23 +79,39 @@ var Movement = [           /*N S E W */
 						   [-1,5,-1,-1], //8
 						   [-1,-1,-1,3], //9
 						   ]
-//create array to enable/disable buttons!
+//create array to enable/disable buttons!, 1 is disable
+var NoNoBtn = [				/*N S E W*/
+							[0,0,0,0],  //0
+							[1,0,0,1],	//1
+							[0,1,0,0],	//2
+							[0,0,0,0],	//3
+							[1,0,0,1],	//4
+							[0,0,1,0],	//5
+							[0,1,1,0],	//6
+							[0,1,0,1],	//7
+							[1,0,1,1],	//8
+							[1,1,1,0],	//9
+							]
 //Directional Buttons
 function btnNorth_click() {
 	nextLoc(North);
 	lookTime();
+	DisableTime();
 }
 function btnSouth_click() {
 	nextLoc(South);
 	lookTime();
+	DisableTime();
 }
 function btnEast_click() {
 	nextLoc(East);
 	lookTime();
+	DisableTime();
 }
 function btnWest_click () {
 	nextLoc(West);
 	lookTime();
+	DisableTime();
 }
 //
 //lookTime function, something new:)
@@ -98,19 +119,34 @@ function lookTime() {
 	var message = "";
 	message = locations[currentLoc].desc;
 	UpdateDisplay(message);
+	if (nextLoc === -1) {
+	UpdateDisplay("Wrong way. Try another Direction!");
+}
 }
 //Introducing basic logic, will be a lot less code than if/else and switch-case
 //create one function to control btn clicks
 function nextLoc(move) {
 	var nextLoc = Movement[currentLoc][move];
+	var message = "Wrong way. Try another Direction!";
 	if (nextLoc >= 0) {
 		currentLoc = nextLoc;
-	} else if (nextLoc < 0) {
-		UpdateDisplay("Wrong way. Try another Direction!");
+	} else if (nextLoc === -1) {
+		UpdateDisplay(message);
 	} 
+	}
+function DisableTime() {
+	var disable = 0;
+	for (j=0; j < btn.length; j++) {
+		disable = NoNoBtn[currentLoc][j];
+	  if (disable === 1) {
+	  document.getElementById(btn[j]).disabled = true;
+	  } else {
+	  document.getElementById(btn[j]).disabled = false;
+	 }
+}
 }
 //Command box
- function btnEnterCommands_click() {
+function btnEnterCommands_click() {
     var message = "Invalid Command. Please try again! Click Need Help? to see valid commands!";
 	var userText = document.getElementById("textinput").value;
 	if (userText === "N" || userText === "n") {
@@ -122,7 +158,7 @@ function nextLoc(move) {
 	} else if (userText === "W" || userText ==="w") {
 		btnWest_click();
 	} else if (userText === "T" || userText ==="t") {
-		take();
+		Take();
 	} else if (userText === "I" || userText ==="i") {
 		checkInventory();
 	} else if (userText === "H" || userText ==="h") {
@@ -148,7 +184,7 @@ function Take() {
 function UpdateDisplay(MSGTA) {
     var textarea = document.getElementById("textarea");
     textarea.value = MSGTA;
- }
+}
 //
 //Function for displaying game-related messages
 function gameMessage(message) {
@@ -158,7 +194,7 @@ function gameMessage(message) {
 function checkInventory() {
 	var playerInventory = "";
 	for (j=0; j < userInventory.length; j++) {
-		playerInventory = playerInventory + userInventory[j];
+		playerInventory = "Your Inventory:" + playerInventory + userInventory[j];
 	}
 	gameMessage(playerInventory);
 }
